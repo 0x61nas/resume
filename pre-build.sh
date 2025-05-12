7fbb22a438dfafa5fddbd0ae710e6b1d05d2e34d
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2025-05-12 21:08:31 +0300

    feat(resume.tex): add more info to the footnote

--- pre-build.sh
+++ pre-build.sh
@@ -12,40 +12,8 @@ source /etc/os-release
 : "${BUILDING_PLATFORM:=$PRETTY_NAME}"
 PDFLATEX_VERSION=$(pdflatex -version | head -n 1)
 
-fail() {
-	echo "Error: " "$@" >/dev/stderr
-	exit 1
-}
-
-log() {
-	echo "$@" >/dev/stderr
-}
-
-if [[ -d "$BUILD_DIR" ]]; then
-	log "Clean up the build dir..."
-	rm -rf $BUILD_DIR/* || fail
-else
-	log "the build directory does not exsist!"
-	log "creating a new build directory.."
-	mkdir $BUILD_DIR || fail
-fi
-
-cp ./resume.tex ./latexmk $BUILD_DIR || fail
-
-cd $BUILD_DIR || fail
-
 sed -i "s/@{GIT_HASH}/$GIT_HASH/g" resume.tex || fail
 sed -i "s/@{GPG_NUMBER_REV}/$GPG_NUMBER_REV/g" resume.tex || fail
 sed -i "s/@{GPG_EMAIL}/$GPG_EMAIL/g" resume.tex || fail
 sed -i "s/@{BUILDING_PLATFORM}/$BUILDING_PLATFORM/g" resume.tex || fail
 sed -i "s/@{PDFLATEX_VERSION}/$PDFLATEX_VERSION/g" resume.tex || fail
-
-latexmk -pdf resume.tex || fail
-
-if [[ ! -d "$DIST_DIR" ]]; then
-	mkdir -p "$DIST_DIR"
-fi
-
-cp resume.pdf "$DIST_DIR/anas-resume-$GIT_HASH.pdf"
-
-echo "anas-resume-$GIT_HASH.pdf" >output_path
