d947c53c27ea1ecce2f42610d5a11c7c80d899e5
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2026-03-11 11:08:30 +0200

    feat(header): interduce the header style system

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ GIT_HASH=$(git rev-parse HEAD)
 GPG_FINGREPRINT="EF4B4CB5DFB8822216A473B1597AB12E66262898"
 GIT_HOST='github\.com\/0x61nas'
 THEME='everforest'
+HEADER_STYLE='aligned'
 BUILD_DIR="$PROOT/build"
 DIST_DIR="$PROOT/dist"
 source /etc/os-release
@@ -39,6 +40,7 @@ sed -i "s/@{GIT_HASH}/$GIT_HASH/g" resume.tex || fail
 sed -i "s/@{GPG_FINGREPRINT}/$GPG_FINGREPRINT/g" resume.tex || fail
 sed -i "s/@{GIT_HOST}/$GIT_HOST/g" resume.tex || fail
 sed -i "s/@{THEME}/$THEME/g" resume.tex || fail
+sed -i "s/@{HEADER_STYLE}/$HEADER_STYLE/g" resume.tex || fail
 sed -i "s/@{BUILDING_PLATFORM}/$BUILDING_PLATFORM/g" resume.tex || fail
 sed -i "s/@{PDFLATEX_VERSION}/$PDFLATEX_VERSION/g" resume.tex || fail
 
